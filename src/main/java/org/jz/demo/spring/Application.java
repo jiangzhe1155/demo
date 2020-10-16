31addb9d4c1568fa3236f5c44ad5c20502bab0fa
jiangzhe <1024741733@qq.com>
2020-10-16 18:55:42 +0800

    测试mysql

--- src/main/java/org/jz/demo/spring/Application.java
+++ src/main/java/org/jz/demo/spring/Application.java
@@ -1,7 +1,17 @@
 package org.jz.demo.spring;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import org.jz.demo.spring.mapper.FileDO;
+import org.jz.demo.spring.mapper.FileMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +20,16 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    FileMapper fileMapper;
+
+    @PostConstruct
+    public void aa() {
+        List<FileDO> fileDOS = fileMapper.selectList(new LambdaQueryWrapper<>());
+        System.out.println(fileDOS);
+
+    }
 }
