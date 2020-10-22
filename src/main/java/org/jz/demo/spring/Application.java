69962f1074ebcbe3443bfb8f286c127401c347f4
jiangzhe <1024741733@qq.com>
2020-10-22 17:05:55 +0800

    feat 添加数据表

--- src/main/java/org/jz/demo/spring/Application.java
+++ src/main/java/org/jz/demo/spring/Application.java
@@ -1,14 +1,9 @@
 package org.jz.demo.spring;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class Application {
@@ -17,12 +12,5 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Autowired
-    DataSource dataSource;
 
-
-    @PostConstruct
-    public void start() {
-
-    }
 }
